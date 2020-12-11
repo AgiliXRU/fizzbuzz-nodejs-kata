bded25602a72704e8632b59901b3252a86b2f041
timmson <timmson666@mail.ru>
2020-12-11 12:21:36 +0300

    Remove "is"

--- test/test-gamer.js
+++ test/test-gamer.js
@@ -7,23 +7,23 @@ describe("Игрок должен", () => {
     const gamer = new Gamer({3: "Fizz", 5: "Buzz"});
 
     it("сказать само число", () => {
-        expect(gamer.answers(1)).is.equals("1");
+        expect(gamer.answers(1)).equals("1");
     });
 
     it("поменять 3 на Fizz", () => {
-        expect(gamer.answers(3)).is.equals("Fizz");
+        expect(gamer.answers(3)).equals("Fizz");
     });
 
     it("поменять число, делящаяся на 3, на Fizz", () => {
-        expect(gamer.answers(6)).is.equals("Fizz");
+        expect(gamer.answers(6)).equals("Fizz");
     });
 
     it("поменять число, делящаяся на 5, на Buzz", () => {
-        expect(gamer.answers(5)).is.equals("Buzz");
+        expect(gamer.answers(5)).equals("Buzz");
     });
 
     it("поменять число, делящаяся на 3 и 5, на FizzBuzz", () => {
-        expect(gamer.answers(15)).is.equals("FizzBuzz");
+        expect(gamer.answers(15)).equals("FizzBuzz");
     });
 
 });
